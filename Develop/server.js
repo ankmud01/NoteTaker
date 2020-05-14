b9dc1ce8c43cbc832deb9660a581b784b2ebae08
Rebel Soul <mudvari.ankit@gmail.com>
2020-05-14 16:59:32 -0400

    updated the static html file location after relocating package.json

--- Develop/server.js
+++ Develop/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 
 //calling the app function from Routes.js files
 require("./routes/apiRoutes")(app);
