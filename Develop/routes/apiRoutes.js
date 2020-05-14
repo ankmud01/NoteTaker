0f337e94946ea1ed2d3b44e6606ffc9048cfcc5a
Ankit Mudvari <mudvari.ankit@gmail.com>
2020-05-14 17:15:17 -0400

    updated comments on the api routes page

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -12,29 +12,29 @@ module.exports = function(app){
     
     //This generates a unique id using shortid package
     const noteId = shortid.generate();
-    //Adding id variable to the newly note thet came from user request
+    //Adding id variable to the new note that came from user request
     newNote.id = noteId;
     // console.log(newNote);
     
-    //Listing the existing notes prior to adding the new note that came from user request
+    //Listing the existing notes prior to adding the new note
     let notes = readNotes;
     // console.log(notes);
 
-    // //Pusing the newnote from request into the file to be written
+    //Push the newnote from request into the file to be written
     notes.push(newNote);
     // console.log(notes);
 
-    //Calls write function to add the newnote into the filesystem
+    //Call write function to add the newnote into the filesystem
     writeNotes(notes);
 
-    //responds with the list of notes added into the file system including the new one
+    //respond with the list of notes added into the file system including the new one
     res.json(notes);
     });
 
     app.delete("/api/notes/:id", function(req, res){
         const deleteNote = req.params;
         let notes = readNotes;
-        //since notes are in array using filter method to remove the matching note id's...
+        //since notes are in array, using filter method to remove the matching note id's...
         const notesToKeep = notes.filter((note) =>note.id !== deleteNote.id)
          // console.log(notesToKeep);
         writeNotes(notesToKeep);
